81b7738757a3b50bba25df7c3c6c600679d74b57
Landon-Robert <159073826+Landon-Robert@users.noreply.github.com>
2024-05-13 13:30:23 -0700

    decided topics

---
+++ Landon.py
@@ -0,0 +1,40 @@
+#calculate the "volume" of a multidimensional square or sphere
+#factor a polynomial
+#turn polynomial into apq form
+#Calculate compound interest
+
+
+def multidim(length,Type,dimensions):
+    if Type = "Circle":
+
+    elif Type = "Square":    
+
+def factor(a,b,c):
+    
+
+def APQ(a,b,c):
+
+
+def Com_Int():
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
