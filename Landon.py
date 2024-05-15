b4f62a26046a737e1d9c6a4bbf237814ce0ea3b4
Landon-Robert <159073826+Landon-Robert@users.noreply.github.com>
2024-05-15 13:29:07 -0700

    Progress

--- Landon.py
+++ Landon.py
@@ -26,26 +26,53 @@ def factor(a,b,c):
         factor = "No solution"
     elif (b**2 - 4*a*c) == 0:
         ans = (-1*b)/2*a
-        ans = 
+        ans = round
         if ans >= 0:
-            factor = f"(x - {ans})"
-        if ans < 0:
-            factor = f"(x + {ans})"
+            factor = f"(x - {ans})^2"
+        elif ans < 0:
+            factor = f"(x + {ans})^2"
     elif (b**2 - 4*a*c) > 0:
-        ans = ((-1*b)*((b**2 - 4*a*c)**0.5))/(2*a)
-            if ans 
-        
-    return factor
+        ans1 = ((-1*b)+((b**2 - 4*a*c)**0.5))/(2*a)
+        ans2 = ((-1*b)-((b**2 - 4*a*c)**0.5))/(2*a)
+        if ans1 >= 0:
+            factor1 = f"(x - {ans1})"
+        elif ans1 < 0: 
+            factor1 = f"(x - {ans1})"
+        if ans2 >= 0:
+            factor2 = f"(x + {ans2})"
+        elif ans2 < 0:
+            factor2 = f"(x + {ans2})"
+        factor = factor1 + factor2
+    return factor 
+
+
 
 def APQ(a,b,c):
-
+    p = b/(-2*a)
+    q = c - (a*(p**2))
+    if a == 1:
+        a = None
+    elif a == -1:
+        a == "-"
+    if p >= 0:
+        ans1 = f"{a}(x - {p})^2"
+    elif p < 0:
+        ans2 = f"{a}(x + {p})^2"
+    if q >= 0:
+        ans2 = f" + {q}"
+    elif q < 0:
+        ans2 = f" - {q}"
+    ans = ans1 + ans2
+    return ans
+    #y = ax^2+bx+c    y = a(x-p)^2+q
 
 def Com_Int():
 
-assert multidim("Square") == 
-assert factor() ==
-assert Com_Int() ==
-assert 
+assert multidim(1,"Square",4) == 1
+assert multidim(1,"Circle",4) == 4.93
+assert factor(1,-5,6) == "(x+5)(x+2)"
+assert APQ(1,1,1) == "(x + 2)^2 - 3"
+assert Com_Int() = 
 
 
 
